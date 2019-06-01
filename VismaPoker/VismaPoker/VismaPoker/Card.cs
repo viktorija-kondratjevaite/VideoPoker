d1027f1952dd858f14f769cbf6635de78d7c13ce
Unknown <viktorija.kondratjevaite@gmail.com>
2019-06-01 17:51:40 +0300

    Add Card and PokerGame classes.

---
+++ VismaPoker/VismaPoker/VismaPoker/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VismaPoker.VismaPoker
+{
+    public class Card
+    {
+        public Card()
+        {
+
+        }
+    }
+}
