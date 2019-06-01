84571ccc500566a2383f8bc2f0d8a88b835f2768
Unknown <viktorija.kondratjevaite@gmail.com>
2019-06-01 17:58:08 +0300

    Implement simple menu

--- VismaPoker/VismaPoker/Program.cs
+++ VismaPoker/VismaPoker/Program.cs
@@ -10,6 +10,31 @@ namespace VismaPoker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Visma Poker Hand Evaluator\nOption list:");
+            while (true)
+            {
+                Console.WriteLine(
+                    "1. Generate a hand\n2. Discard and replace a card.\n3. Evaluate a hand and get a prize.");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Hand Generated");
+                        break;
+                    case "2":
+                        Console.WriteLine("Which card do you want to replace? (0-4)");
+                        var cardNumber = Console.ReadLine();
+                        Console.WriteLine("Card number " + cardNumber + " was replaced");
+                        Console.WriteLine("Do you want to replace another card? (Y/N)");
+                        break;
+                    case "3":
+                        Console.WriteLine("Hand evaluated");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 }
