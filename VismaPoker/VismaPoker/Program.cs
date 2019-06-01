5261859ede527647d3ba9c0ca17fb9f90f051e80
Unknown <viktorija.kondratjevaite@gmail.com>
2019-06-01 18:19:33 +0300

    Change menu to use new PokerGame and PokerHandEvaluator methods

--- VismaPoker/VismaPoker/Program.cs
+++ VismaPoker/VismaPoker/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VismaPoker.VismaPoker;
+using VismaPoker.VismaPoker.Interfaces;
 
 namespace VismaPoker
 {
@@ -11,6 +13,7 @@ namespace VismaPoker
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Visma Poker Hand Evaluator\nOption list:");
+            IPokerGame pokerGame = new PokerGame();
             while (true)
             {
                 Console.WriteLine(
@@ -19,16 +22,21 @@ namespace VismaPoker
                 switch (input)
                 {
                     case "1":
+                        pokerGame.GenerateHand();
                         Console.WriteLine("Hand Generated");
+                        pokerGame.PrintHand();
                         break;
                     case "2":
                         Console.WriteLine("Which card do you want to replace? (0-4)");
                         var cardNumber = Console.ReadLine();
+                        pokerGame.ReplaceCard(int.Parse(cardNumber));
                         Console.WriteLine("Card number " + cardNumber + " was replaced");
+                        pokerGame.PrintHand();
                         Console.WriteLine("Do you want to replace another card? (Y/N)");
                         break;
                     case "3":
-                        Console.WriteLine("Hand evaluated");
+                        pokerGame.EvaluateHand();
+                        pokerGame.ClearHand();
                         break;
                     default:
                         break;
