98bee7208e70dd5f5a4d727f944bde4c62e361dd
Unknown <viktorija.kondratjevaite@gmail.com>
2019-06-01 18:51:39 +0300

    Move Menu to separate class.

--- VismaPoker/VismaPoker/Program.cs
+++ VismaPoker/VismaPoker/Program.cs
@@ -12,36 +12,8 @@ namespace VismaPoker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Visma Poker Hand Evaluator\nOption list:");
-            IPokerGame pokerGame = new PokerGame();
-            while (true)
-            {
-                Console.WriteLine(
-                    "1. Generate a hand\n2. Discard and replace a card.\n3. Evaluate a hand and get a prize.");
-                var input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        pokerGame.GenerateHand();
-                        Console.WriteLine("Hand Generated");
-                        pokerGame.PrintHand();
-                        break;
-                    case "2":
-                        Console.WriteLine("Which card do you want to replace? (0-4)");
-                        var cardNumber = Console.ReadLine();
-                        pokerGame.ReplaceCard(int.Parse(cardNumber));
-                        Console.WriteLine("Card number " + cardNumber + " was replaced");
-                        pokerGame.PrintHand();
-                        Console.WriteLine("Do you want to replace another card? (Y/N)");
-                        break;
-                    case "3":
-                        pokerGame.EvaluateHand();
-                        pokerGame.ClearHand();
-                        break;
-                    default:
-                        break;
-                }
-            }
+            PokerMenu pokerMenu = new PokerMenu();
+            pokerMenu.StartPokerGame();
 
         }
     }
---
+++ VismaPoker/VismaPoker/VismaPoker/PokerMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VismaPoker.VismaPoker.Interfaces;
+
+namespace VismaPoker.VismaPoker
+{
+    public class PokerMenu
+    {
+
+        public void StartPokerGame()
+        {
+            Console.WriteLine("Welcome to the Visma Poker Hand Evaluator\nOption list:");
+            IPokerGame pokerGame = new PokerGame();
+            while (true)
+            {
+                Console.WriteLine(
+                    "1. Generate a hand\n2. Discard and replace a card.\n3. Evaluate a hand and get a prize.");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        pokerGame.GenerateHand();
+                        Console.WriteLine("Hand Generated");
+                        pokerGame.PrintHand();
+                        break;
+                    case "2":
+                        Console.WriteLine("Which card do you want to replace? (0-4)");
+                        var cardNumber = Console.ReadLine();
+                        pokerGame.ReplaceCard(int.Parse(cardNumber));
+                        Console.WriteLine("Card number " + cardNumber + " was replaced");
+                        pokerGame.PrintHand();
+                        Console.WriteLine("Do you want to replace another card? (Y/N)");
+                        break;
+                    case "3":
+                        pokerGame.EvaluateHand();
+                        pokerGame.ClearHand();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
